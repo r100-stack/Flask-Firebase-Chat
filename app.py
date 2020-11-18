a622d56413550b1b33d6cd0d644bc4e19851e511
Rohan Kadkol <rohan-kadkol@users.noreply.github.com>
2020-11-18 08:59:07 -0500

    Added TODOs for step 2

--- app.py
+++ app.py
@@ -1,22 +1,14 @@
-# TODO (1): Import the necessary libraries
-
 from flask import Flask, jsonify, render_template, request, redirect
 from sqlalchemy import create_engine
 from data import sample_messages
-
-# TODO (2): Setup the app, engine, and connection
 
 app = Flask(__name__)
 engine = create_engine('mysql://kali:kali@localhost:3306/flaskchat')
 conn = engine.connect()
 
-# TODO (3): Render the index.html template for the / endpoint
-
 @app.route('/')
 def index():
     return render_template("index.html")
-
-# TODO (4): Return the sample messages as a json for the /sample_messages endpoint
 
 @app.route('/sample_messages')
 def get_sample_messages():
@@ -26,7 +18,27 @@ def get_sample_messages():
 def get_messages():
     return jsonify('')
 
+    # TODO (1): Get the messages using the SQL statement: select * from messages order by id desc;
+    # TODO (2): Parse the results into an list of dictionaries (JSON objects)
+    # TODO (3): Return the list
+
+    # results = conn.execute("select * from messages order by id desc;")
+    # messages = []
+    # for row in results:
+    #     messages.append({
+    #         'id': row['id'],
+    #         'message': row['message'],
+    #         'sender': row['sender']
+    #     })
+    # return jsonify(messages)
+
 @app.route('/messages', methods=['POST'])
 def post_message():
     return jsonify('')
+
+    # TODO (4): Insert the message into the database
+    # TODO (5): Redirect to the /messages endpoint
+
+    # conn.execute('insert into messages (message, sender) values (%s, %s)', (request.get_json()['message'], request.get_json()['sender']))
+    # return redirect('/messages')
 
