fc7427f6e452f7a6bca0c95cc276c1d440ee9c77
Rohan Kadkol <rohan-kadkol@users.noreply.github.com>
2020-11-18 09:09:52 -0500

    Added TODOs for step 3

--- app.py
+++ app.py
@@ -16,12 +16,6 @@ def get_sample_messages():
 
 @app.route('/messages', methods=['GET'])
 def get_messages():
-    # return jsonify('')
-
-    # TODO (1): Get the messages using the SQL statement: select * from messages order by id desc;
-    # TODO (2): Parse the results into an list of dictionaries (JSON objects)
-    # TODO (3): Return the list
-
     results = conn.execute("select * from messages order by id desc;")
     messages = []
     for row in results:
@@ -34,11 +28,6 @@ def get_messages():
 
 @app.route('/messages', methods=['POST'])
 def post_message():
-    # return jsonify('')
-
-    # TODO (4): Insert the message into the database
-    # TODO (5): Redirect to the /messages endpoint
-
     conn.execute('insert into messages (message, sender) values (%s, %s)', (request.get_json()['message'], request.get_json()['sender']))
     return redirect('/messages')
 
