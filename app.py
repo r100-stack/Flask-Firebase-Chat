80761927e3f58ebc7122b355ff45ee7e3cbf2f26
Rohan Kadkol <rohan-kadkol@users.noreply.github.com>
2020-11-18 09:00:42 -0500

    Added solutions for Step 2

--- app.py
+++ app.py
@@ -16,29 +16,29 @@ def get_sample_messages():
 
 @app.route('/messages', methods=['GET'])
 def get_messages():
-    return jsonify('')
+    # return jsonify('')
 
     # TODO (1): Get the messages using the SQL statement: select * from messages order by id desc;
     # TODO (2): Parse the results into an list of dictionaries (JSON objects)
     # TODO (3): Return the list
 
-    # results = conn.execute("select * from messages order by id desc;")
-    # messages = []
-    # for row in results:
-    #     messages.append({
-    #         'id': row['id'],
-    #         'message': row['message'],
-    #         'sender': row['sender']
-    #     })
-    # return jsonify(messages)
+    results = conn.execute("select * from messages order by id desc;")
+    messages = []
+    for row in results:
+        messages.append({
+            'id': row['id'],
+            'message': row['message'],
+            'sender': row['sender']
+        })
+    return jsonify(messages)
 
 @app.route('/messages', methods=['POST'])
 def post_message():
-    return jsonify('')
+    # return jsonify('')
 
     # TODO (4): Insert the message into the database
     # TODO (5): Redirect to the /messages endpoint
 
-    # conn.execute('insert into messages (message, sender) values (%s, %s)', (request.get_json()['message'], request.get_json()['sender']))
-    # return redirect('/messages')
+    conn.execute('insert into messages (message, sender) values (%s, %s)', (request.get_json()['message'], request.get_json()['sender']))
+    return redirect('/messages')
 
