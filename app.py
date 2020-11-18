bc18622a745a8b8a65e5c477fc2cf22a79dee4d6
Rohan Kadkol <rohan-kadkol@users.noreply.github.com>
2020-11-18 08:52:51 -0500

    Added solutions for step 1

--- app.py
+++ app.py
@@ -1,26 +1,26 @@
 # TODO (1): Import the necessary libraries
 
-# from flask import Flask, jsonify, render_template, request, redirect
-# from sqlalchemy import create_engine
-# from data import sample_messages
+from flask import Flask, jsonify, render_template, request, redirect
+from sqlalchemy import create_engine
+from data import sample_messages
 
 # TODO (2): Setup the app, engine, and connection
 
-# app = Flask(__name__)
-# engine = create_engine('mysql://kali:kali@localhost:3306/flaskchat')
-# conn = engine.connect()
+app = Flask(__name__)
+engine = create_engine('mysql://kali:kali@localhost:3306/flaskchat')
+conn = engine.connect()
 
 # TODO (3): Render the index.html template for the / endpoint
 
-# @app.route('/')
-# def index():
-#     return render_template("index.html")
+@app.route('/')
+def index():
+    return render_template("index.html")
 
 # TODO (4): Return the sample messages as a json for the /sample_messages endpoint
 
-# @app.route('/sample_messages')
-# def get_sample_messages():
-#     return jsonify(sample_messages)
+@app.route('/sample_messages')
+def get_sample_messages():
+    return jsonify(sample_messages)
 
 @app.route('/messages', methods=['GET'])
 def get_messages():
