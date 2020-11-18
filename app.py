6b202865d00b9dc0e423d879f854a3ea73484f18
Rohan Kadkol <rohan-kadkol@users.noreply.github.com>
2020-11-18 08:43:00 -0500

    Flask is working

--- app.py
+++ app.py
@@ -28,6 +28,18 @@ def get_messages():
 
 @app.route('/messages', methods=['POST'])
 def post_message():
-    conn.execute('insert into messages (message, sender) values (%s, %s)', (request.form['message'], request.form['sender']))
-    return redirect('/')
+    # print(request.data)
+    # print(type(request.data))
+    # print(request.get_json())
+    # print(type(request.get_json()))
+    # print(request.json)
+    # print(type(request.json))
+
+    # req_data = request.get_json()
+    # print(req_data)
+
+    # body = request.get_json
+
+    conn.execute('insert into messages (message, sender) values (%s, %s)', (request.get_json()['message'], request.get_json()['sender']))
+    return redirect('/messages')
 
