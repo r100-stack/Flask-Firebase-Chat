244407fd0d81a0295ebc2f066b9d0295e5962d19
Rohan Kadkol <rohan-kadkol@users.noreply.github.com>
2020-12-18 15:56:46 -0600

    Removed exception reached and conn closed. TODO implemented: Remove exception rchd and conn closed comments

--- error_handling.py
+++ error_handling.py
@@ -1,21 +1,18 @@
 from flask import jsonify
 from init_flask import engine
 
-# TODO: Remove exception rchd and conn closed comments
 def get_connection_and_handle_error(func):
     def try_catch():
         try:
             conn = engine.connect()
             response = func(conn=conn)
         except Exception as ex:
-            print('Exception reached')
             print(ex)
             response = jsonify({
                 'success': False,
                 'error': str(ex)
             }), 500
         finally:
-            print('Connection closed')
             conn.close()
         return response
     try_catch.__name__ = func.__name__
