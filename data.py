ed8b65d793ba58b9eb63e9a4d1ab3b9d25a680fa
Rohan Kadkol <rohan-kadkol@users.noreply.github.com>
2020-12-19 17:14:52 -0600

    Updated sample_messages in data.py. ie. Added missing ID field in each message.

--- data.py
+++ data.py
@@ -1,9 +1,11 @@
 sample_messages = [
     {
+        'ID': 4,
         'message': 'Hello World!',
         'sender': 'Guido van Rossum'
     },
     {
+        'ID': 5,
         'message': 'I love Baton Rouge',
         'sender': 'Joe Burrow'
     }
