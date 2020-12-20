431cb97384c6534070d4ff2ab6daa21ea6988865
Rohan Kadkol <rohan-kadkol@users.noreply.github.com>
2020-12-19 18:05:48 -0600

    Added a few extra TODOs with numbers for init_flask.py.

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-# TODO (5): from flask import Flask, render_template
-# TODO (6): from init_flask import app
+# TODO (9): from flask import Flask, render_template
+# TODO (10): from init_flask import app
 from flask import Flask, render_template
 from init_flask import app
 
-# TODO (7): Use @app.route to accept GET /
-# TODO (8): Create a method called index
-# TODO (9): Have index() return a render_template('index.html')
+# TODO (11): Use @app.route to accept GET /
+# TODO (12): Create a method called index
+# TODO (13): Have index() return a render_template('index.html')
 @app.route('/', methods=['GET'])
 def index():
     """GET / - Endpoint to display the homepage HTML file."""
